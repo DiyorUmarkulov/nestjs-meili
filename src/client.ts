6f08f82826a8c21cf1bccd46524055e1f6a186bf
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2025-05-02 18:06:51 +0500

    fix: index initialization

--- src/client.ts
+++ src/client.ts
@@ -20,7 +20,7 @@ export class MeiliClient {
     try {
       await index.getStats();
     } catch (error) {
-      if (error.message.includes("has failed")) {
+      if (error.message.includes("not found")) {
         await this.client.createIndex(name, { primaryKey });
         index = this.client.index(name);
       } else {
